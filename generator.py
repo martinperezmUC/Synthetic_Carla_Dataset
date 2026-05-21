297ddc74e3a6d3ea64c9a986f3cd993dbb1b663f
martinperezmUC <martinperezm@unican.es>
2026-05-21 19:46:48 +0200

    Primera versión funcional para dataset sintético de sensores basados en CARLA.

--- generator.py
+++ generator.py
@@ -8,89 +8,156 @@ by CARLA Simulator. Currently supports the generation of data for the following 
 - Radar
 
 @author: Mario Martín <martinperezm@unican.es>
-@version: 0.1
+@version: 0.2
 """
 
 import json
-import os
+import argparse
 import numpy as np
 
-CURRENT_VERSION = 0.1
-WELCOME_MESSAGE = ('Welcome to the CARLA-like synthetic data generation tool.'
-    '\nYou will now see some options to personalize the format of your data. '
-    'Please, take into account, that this data is completely random and is '
-    'for pure benchmarking tests only.\n'
+MAX_PRECISION = 15
+MIN_PRECISION = 1
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Creates a synthetic dataset emulating CARLA sensors.'
+    )
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument(
+        '--minutes', '-m',
+        type = int,
+        help = 'Time in minutes.'
+    )
+    group.add_argument(
+        '--frames', '-f',
+        type = int,
+        help = 'Number of frames'
+    )
+    parser.add_argument(
+        '--precision', '-p',
+        type = int,
+        default = 6,
+        choices = range(MIN_PRECISION, MAX_PRECISION + 1),
+        metavar = f'[{MIN_PRECISION}, {MAX_PRECISION}]',
+        help = 'Float precision (64b) or number of decimals.'
+    )
+    parser.add_argument(
+        '--output', '-o',
+        default = 'dataset.json',
+        help = 'Name of JSON File.'
+    )
+    parser.add_argument(
+        '--interactive', '-i',
+        action = 'store_true',
+        help = 'Use interactive mode'
     )
 
-MAX_DECIMALS = 30
+    args = parser.parse_args()
 
-num_frames = 0
-num_decimals = 0
-filename = ''
+    if not args.interactive and args.minutes is None and args.frames is None:
+        parser.error('Either --minutes/-m or --frames/-f is required (unless using --interactive/-i)')
 
-def welcome():
+    return args
+
+def args_to_settings(args):
+    if args.minutes is not None:
+        num_frames = args.minutes * 60
+    else:
+        num_frames = args.frames
+    filename = args.output
+    if not filename.endswith('.json'):
+        filename += '.json'
+
+    return num_frames, args.precision, filename
+
+def ask_int(prompt, min_val = None, max_val = None):
     """
-    Welcome the user, describe the program and ask if the user desires a default and simple configuration.
+    Helper to verify an integer from the user.
     """
-    print(WELCOME_MESSAGE)
+    while True:
+        try:
+            value = int(input(f'  >>> {prompt} ' ))
+            if min_val and value < min_val:
+                print(f'  [!] Minimum value: {min_val}')
+                continue
+            if max_val and value > max_val:
+                print(f'  [!] Maximum value: {max_val}')
+                continue
+            return value
+        except ValueError:
+            print('  [!] Plese, enter a valid value.')
+
+def ask_str(prompt, default = None):
+    """
+    Helper to ask for a string from the user.
+    """
+    if default:
+        default_str = f'  [{default}]'
+    else:
+        default_str = ''
+   
+    value = input(f'  >>> {prompt}{default_str} ').strip()
+    return value or default
     
-    print('Author: Mario Martin-Perez <martinperezm@unican.es>')
-    print(f'Version: {CURRENT_VERSION}\n\n')
-    
-def tune_generation():
-    print('+ What unit of measurement do you wish to use in order to set the number of frames to generate?:')
-    print('\t1) Time in minutes (experimental - only 1 frame per second supported).')
-    print('\t2) Number of frames.')
-    print('\tChoose [1-2]:')
-    sel_1 = int(input())
+def interactive_tune():
+    """
 
-    match (sel_1):
-        case 1:
-            print('\tTime in minutes selected. Please, indicate the time [1-10]:')
-            time = int(input())
-            if (time < 1 or time > 10):
-                print('\tTime is out of [1-10] bounds.')
-                exit()
-            else:
-                num_frames = time * 60
+    """
+    print('\n' + '='*30)
+    print('  GENERATOR SETTINGS')
+    print('='*30 + '\n')
 
-        case 2:
-            print('\tNumber of frames selected. Please, indicate the number [1-inf)')
-            num_frames = int(input())
-            if (num_frames > 0):
-                print(f'\tNumber of frames: {num_frames}')
-            else:
-                print('\tInvalid value.')
-                exit()
+    # Minutes/Frames selector
+    print('Select time unit:')
+    print('  [1] Time in minutes (experimental - 1 fps)')
+    print('  [2] Number of frames')
+    sel = ask_int('Option [1-2]:', 1, 2)
 
-        case _:
-            print(f'Selection = {sel_1}. Closing...')
-            exit()
+    if sel == 1:
+        minutes = ask_int('Minutes [1-59]:', 1, 59)
+        num_frames = minutes * 60
+        print(f'  [+] Settings: {minutes} min --> {num_frames} frames\n')
+    else:
+        num_frames = ask_int('Frames [>0]:', 1)
+        print(f'  [+] Settings: {num_frames} frames\n')
 
-    print('+ How many decimal characters do you want [1, 30]?:')
-    num_decimals = int(input())
-    if (num_decimals < 1 or num_decimals > MAX_DECIMALS):
-        print('\tDecimals are out of [1-30] bounds.')
-        exit()
-    else: 
-        print(f'\tNumber of decimals: {num_decimals}. Example: {round(np.random.uniform(1,10), num_decimals)}')
+    # Precision
+    print('Decimal precision:')
+    print(f'  Range: {MIN_PRECISION}-{MAX_PRECISION}')
+    precision = ask_int(f'Precision {MIN_PRECISION}-{MAX_PRECISION}:', MIN_PRECISION, MAX_PRECISION)
+    example = round(np.random.uniform(MIN_PRECISION, MAX_PRECISION), precision)
+    print(f'  [+] Example: {example}\n')
 
-    print('Please, choose a filename for your json (.json will be added automatically). Choose it carefully, ' +
-          'since this is the last user input allowed. \n IMPORTANT: NO FORMAT VERIFICATION IS MADE!')
-    filename = input() + '.json'
-    print(f'Data will be located on ./{filename}')
+    # Filename
+    print('JSON Filename:')
+    print('  (.json extension will be added automatically)')
+    filename = ask_str('File:', 'dataset')
+    if not filename.endswith('.json'):
+        filename += '.json'
+    print(f'  [+] Save in: ./{filename}\n')
 
-def gen_dec_val():
-    return round(np.random.uniform(10.0, 99.9), num_decimals)
+    # Settings
+    print('=' * 30)
+    print('  SETTINGS')
+    print('=' * 30)
+    print(f'  * Frames:    {num_frames} frames')
+    print(f'  * Precision: {precision} decimals')
+    print(f'  * Filename:  {filename}')
+    print('=' * 30)
 
-def generate_synthetic_data():
+    return num_frames, precision, filename
+
+def gen_dec_val(precision):
+    return round(np.random.uniform(10.0, 99.9), precision)
+
+def generate_synthetic_data(num_frames, precision, filename):
     """
     Generate synthetic data for the supported sensors. The data is generated based on the 
     sensor type and the user's input. The generated data is saved in a JSON file.
     """
     print('Starting simulation with: ')
     print(f'--> {num_frames} frames.')
-    print(f'--> {num_decimals} decimales.')
+    print(f'--> {precision} decimales.')
     print(f'--> Exporting to: {filename}')
     print('-' * 40 + '\n')
 
@@ -101,22 +168,22 @@ def generate_synthetic_data():
             'frame': int(frame),
             'sensors': {
                 'gnss': {
-                    'latitude': gen_dec_val(),
-                    'longitude':gen_dec_val(),
-                    'altitude': gen_dec_val()
+                    'latitude': gen_dec_val(precision),
+                    'longitude':gen_dec_val(precision),
+                    'altitude': gen_dec_val(precision)
                 },
                 'imu': {
                     'accelerometer': {
-                        'x': gen_dec_val(),
-                        'y': gen_dec_val(),
-                        'z': gen_dec_val()
+                        'x': gen_dec_val(precision),
+                        'y': gen_dec_val(precision),
+                        'z': gen_dec_val(precision)
                     },
                     'gyroscope': {
-                        'x': gen_dec_val(),
-                        'y': gen_dec_val(),
-                        'z': gen_dec_val()
+                        'x': gen_dec_val(precision),
+                        'y': gen_dec_val(precision),
+                        'z': gen_dec_val(precision)
                     },
-                    'compass': gen_dec_val 
+                    'compass': gen_dec_val(precision)
                 },
                 'CameraRGB': {
                     'x': 1920,
@@ -131,18 +198,23 @@ def generate_synthetic_data():
                 'LiDAR': {
                     'num_points': int(np.random.randint(10, 99)),
                     'horizontal_angle': 20,
-                    'cloudUri': f'C:Vehicle/lidar_cloud_{frame:08d}.bin'
+                    'cloudUri': f'C:/Vehicle/lidar_cloud_{frame:08d}.bin'
                 }
             }
         }
         dataset.append(frame_data)
     
-    with open(('./' + filename), 'w') as f:
+    with open(filename, 'w') as f:
         json.dump(dataset, f, indent=4)
 
     print('Dataset generated.')
 
 if __name__ == "__main__":
-    welcome()
-    tune_generation()
-    generate_synthetic_data()
+    args = parse_args()
+
+    if args.interactive:
+        num_frames, precision, filename = interactive_tune()
+    else:
+        num_frames, precision, filename = args_to_settings()
+
+    generate_synthetic_data(num_frames, precision, filename)
