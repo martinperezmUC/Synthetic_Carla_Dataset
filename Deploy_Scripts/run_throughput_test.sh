07aa3b425d66d1be8981711fcbe88b37bc414fa7
martinperezmUC <martinperezm@unican.es>
2026-06-11 13:59:03 +0200

    Bug fixes

--- Deploy_Scripts/run_throughput_test.sh
+++ Deploy_Scripts/run_throughput_test.sh
@@ -5,7 +5,7 @@ LOG_FILE="throughput_log_$(date +%Y%m%d_%H%M%S).txt"
 
 mapfile -t nodes < nodes_dds.txt
 
-if [ ${#nodes[@]} -lt ]; then
+if [ ${#nodes[@]} -lt 2 ]; then
     echo "[!] Error: 2 or more nodes are needed on nodes_dds.txt".
     exit 1
 fi 
